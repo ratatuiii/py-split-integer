e398c71c0b1a4dd980c80078f4645787539c705e
naomi <orest.chupa@gmail.com>
2026-04-17 17:42:37 +0300

    Resolve stylistic errors

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,39 +2,19 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    argument = 11223344
-
-    result = sum(split_integer(argument, 4))
-    expected = argument
-
-    assert result == expected
+    assert sum(split_integer(11223344, 4)) == 11223344
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    argument = 64
-
-    result = split_integer(argument, 8)
-    expected = [8] * 8
-
-    assert result == expected
+    assert split_integer(64, 8) == [8] * 8
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    argument = 11
-
-    result = split_integer(argument, 1)
-    expected = [11]
-
-    assert result == expected
+    assert split_integer(11, 1) == [11]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    argument = 17
-
-    result = split_integer(argument, 4)
-    expected = [4, 4, 4, 5]
-
-    assert result == expected
+    assert split_integer(17, 4) == [4, 4, 4, 5]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
